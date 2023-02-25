8f9241dc5f3829d9e83f611dcf31b2c8513ec451
Lutfi Fadil Lokman <47475707+LutfiLokman@users.noreply.github.com>
2023-02-25 07:24:02 -0500

    auto-push

--- NLP/lstm_learn.py
+++ NLP/lstm_learn.py
@@ -1,5 +1,26 @@
-import tensorflow as tf
 from tensorflow.keras.datasets import imdb
+from tensorflow.keras.layers import LSTM, Dense, Embedding
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing import sequence
 
 top_words = 5000
-(X_train, y_train), (X_test, y_test) = imdb.load.data(num_words=top_words)
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+
+max_review_length = 500
+
+X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
+X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
+
+embedding_vector_length = 32
+
+model = Sequential()
+model.add(Embedding(top_words, embedding_vector_length, input_length=max_review_length))
+model.add(LSTM(100))
+model.add(Dense(1, activation="sigmoid"))
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+print(model.summary())
+
+model.fit(X_train, y_train, epochs=3, batch_size=64)
+
+scores = model.evaluate(X_test, y_test, verbose=0)
